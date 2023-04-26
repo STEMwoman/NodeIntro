655e39b80eaa9df76d9928434e74d8a74757aec1
COOPER <ac0932091@dt038560.otc.edu>
2023-04-26 13:06:37 -0500

    added movie db API

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ import {configs} from './scripts.js'
 scripts();
 configs();
 
-axios.get("https://dog.ceo/api/breeds/image/random")
+import {config} from 'dotenv'
+config();
+console.log(process.env.API_KEY);
+
+let api_url =
+`
+https://api.themoviedb.org/3/movie/now_playing?api_key=f9d083f7bfde69e00d56a00341d8a023&language=en-US&page=1
+`
+axios.get(api_url)
 .then (res=>(
     console.log(res.data)
-));
+))
